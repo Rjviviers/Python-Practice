df2bf603961f72e845a6eccd3f8d24465f300a8c
Ruan Viviers <rjviviers@gmail.com>
2019-02-14 12:49:38 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,139 @@
+# students_count = 1000
+# rating = 4.99
+# multiline = """
+# hello
+# my name
+# jeff
+# """
+# print(type(students_count))
+# print(id(rating))
+# x = 1
+# x = x+1
+# print(id(x))
+
+
+# course = "Python practice"
+# print(len(course))
+# print(course[0])
+# print(course[-2])
+# print(course[0: 3])
+
+# print(id(course))
+
+# print(id(course[0]))
+
+#message = "python \" programming"
+
+# first = "ruan"
+# last = "viviers"
+# full = f"{first} {last}"
+# print(full)
+
+# course = " pyton programming"
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.strip())
+
+# x = 10
+# print(bin(x))
+
+age = 22
+# if age >= 18:
+#     print("adult")
+# elif age >= 13:
+#     print("teenager")
+# else:
+#     print("child")
+# print("all done")
+
+# name = " "
+# if not name.strip:
+#     print("name is empty")
+
+# if 18 <= age < 65:
+#     print("elegeble")
+
+# message = "elegible" if age >= 18 else "not elegeble"
+# print(message)
+
+# for x in "python":
+#     print(x)
+# for x in ['a', 'b', 'c']:
+#     print(x)
+# for x in range(0, 10, 2):
+#     print(x)
+
+# names = ["john", "mary"]
+# found = False
+# for name in names:
+#     if name.startswith("j"):
+#         print("found")
+#         found = True
+#         break
+# if not found:
+#     print("not found")
+
+# names = ["john", "mary"]
+
+# for name in names:
+#     if name.startswith("j"):
+#         print("found")
+
+#         break
+# else:
+#     print("not found")
+
+# guess = 0
+# awnser = 5
+
+# while awnser != guess:
+#     guess = int(input("guess: "))
+# else:
+#     pass
+
+
+# def increment(number, by=1):
+#     return (number, number + by)
+
+
+# print(increment(2))
+
+
+# def multiply(*list):
+#     total = 1
+#     for item in list:
+#         total *= item
+#     return total
+
+
+# print(multiply(2, 3, 4, 5))
+
+
+# def save_user(**user):
+#     print(user)
+
+
+# save_user(id=1, name="admin")
+
+# message = "a"
+
+
+# def greet():
+#     global message
+#     message = "b"
+
+
+# greet()
+# print(message)
+
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+    return total
+
+
+print("start")
+print(multiply(1, 2, 3))
+print("finish")
